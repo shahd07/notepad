5834b8691457b91056f8288c15614c4c4da6dcbe
shahd07 <142664788+shahd07@users.noreply.github.com>
2023-08-29 00:54:08 +0300

    Add files via upload

---
+++ notepad.py
@@ -0,0 +1,7 @@
+import sys, os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, \
+                            QPushButton, QLabel, QPlainTextEdit, QStatusBar, QToolBar, \
+                            QVBoxLayout, QAction, QFileDialog, QMessageBox
+from PyQt5.QtCore import Qt, QSize                          
+from PyQt5.QtGui import QFontDatabase, QIcon, QKeySequence
+from PyQt5.QtPrintSupport import QPrintDialog
